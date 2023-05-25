4b377e064bb638ae1598657844ad23a6167a00d5
Tridev Verma <tv072000@gmail.com>
2023-05-25 23:44:23 +0530

    remove show class

--- src/ToolTip.jsx
+++ src/ToolTip.jsx
@@ -2,14 +2,7 @@ import React from "react";
 
 const ToolTip = ({ pos, text }) => {
   return (
-    <button
-      className="tooltip-btn"
-      data-content={pos}
-      onMouseEnter={(e) => {
-        e.currentTarget.classList.add("show");
-      }}
-      onMouseLeave={(e) => e.currentTarget.classList.remove("show")}
-    >
+    <button className="tooltip-btn" data-content={pos}>
       {text}
     </button>
   );
